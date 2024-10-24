4848fac1cbc72be0bce1a0ba4fa348dea5aa38a0
Ramish Imran <rmm24stg@bangor.ac.uk>
2024-10-24 13:52:36 +0100

    commit

--- titanic_analysis/numerical_df.py
+++ titanic_analysis/numerical_df.py
@@ -1,12 +1,13 @@
+import pandas as pd
+
 def get_numerical_df(df, numerical_features):
-    """
-    Creates a DataFrame containing only numerical features.
-    
-    Args:
-        df (pd.DataFrame): The Titanic dataset as a DataFrame.
-        numerical_features (list): List of numerical feature names.
-    
-    Returns:
-        pd.DataFrame: DataFrame containing only numerical features.
-    """
-    pass  # Implement the logic here
+    #filter dataframe
+    numerical_df = df[numerical_features].copy()
+    return numerical_df
+
+# testing function's executions and outputs
+if _name_ == "_main_":
+    filepath = "data/titanic.csv" 
+    titanic_data = pd.read_csv(filepath)
+    numerical_features = ['Age', 'Fare']
+    numerical_df = get_numerical_df(titanic_data, numerical_features)
